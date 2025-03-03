0ceb10a62384119e170c0c7f79bfca8edfbd4a04
Gastons2023 <kechara.neal@gmail.com>
2025-03-03 18:31:38 -0500

    finalize my code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ const Footer = () => {
         <footer>
             <div>
                 <a href="https://github.com/Gastons2023" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/ke-chara-n-24893b266/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
                 <a href="https://stackoverflow.com/users/28189798/kechara-neal" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-stack-overflow"></i></a>
             </div>
             <p>&copy; {new Date().getFullYear()} Ke'Chara Neal. All rights reserved.</p>
